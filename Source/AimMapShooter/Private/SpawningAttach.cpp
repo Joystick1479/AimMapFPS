9a5db4f416821a196fe067fc84d8565189af3a77
barwen1899 <bartek.jj@gmail.com>
2019-12-20 09:57:50 +0000

    Created random spawn items actor. Multiplayer replicated.

---
+++ Source/AimMapShooter/Private/SpawningAttach.cpp
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawningAttach.h"
+#include "HoloScope.h"
+#include "Helmet.h"
+#include "Headset.h"
+#include "Grip.h"
+#include "Laser.h"
+
+// Sets default values
+ASpawningAttach::ASpawningAttach()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetReplicates(true);
+
+}
+
+// Called when the game starts or when spawned
+void ASpawningAttach::BeginPlay()
+{
+	Super::BeginPlay();
+	Actors.Push(Holo);
+	Actors.Push(Headset);
+	Actors.Push(Helmet);
+	Actors.Push(Grip);
+	Actors.Push(Laser);
+
+	SpawnRandom();
+}
+
+void ASpawningAttach::SpawnRandom()
+{
+	if (Role < ROLE_Authority)
+	{
+		ServerSpawnRandom();
+		return;
+	}
+	int32 range = Actors.Num();
+	UE_LOG(LogTemp, Warning, TEXT("Numbers: %i"), range);
+	int32 randomNumber = FMath::RandRange(0, range);
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	UE_LOG(LogTemp, Warning, TEXT("RandomNumbers: %i"), randomNumber);
+
+	switch (randomNumber) {
+	case 1:
+		UE_LOG(LogTemp, Warning, TEXT("HoloSpawn"));
+		Holo = GetWorld()->SpawnActor<AHoloScope>(HoloClass, GetActorLocation(), GetActorRotation(), SpawnParams);
+		break;
+	case 2:
+		UE_LOG(LogTemp, Warning, TEXT("Headset"));
+		Helmet = GetWorld()->SpawnActor<AHelmet>(HelmetClass, GetActorLocation(), GetActorRotation(), SpawnParams);
+		break;
+	case 3:
+		UE_LOG(LogTemp, Warning, TEXT("Helmet"));
+		Headset = GetWorld()->SpawnActor<AHeadset>(HeadsetClass, GetActorLocation(), GetActorRotation(), SpawnParams);
+		break;
+	case 4:
+		UE_LOG(LogTemp, Warning, TEXT("Grip"));
+		Laser = GetWorld()->SpawnActor<ALaser>(LaserClass, GetActorLocation(), GetActorRotation(), SpawnParams);
+		break;
+	case 5:
+		UE_LOG(LogTemp, Warning, TEXT("Laser"));
+		Grip = GetWorld()->SpawnActor<AGrip>(GripClass, GetActorLocation(), GetActorRotation(), SpawnParams);
+		break;
+	}
+
+
+}
+
+void ASpawningAttach::ServerSpawnRandom_Implementation()
+{
+	SpawnRandom();
+}
+bool ASpawningAttach::ServerSpawnRandom_Validate()
+{
+	return true;
+}
+
+// Called every frame
+void ASpawningAttach::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/AimMapShooter/Public/SpawningAttach.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawningAttach.generated.h"
+
+class AHelmet;
+class AHeadset;
+class AGrip;
+class AHoloScope;
+class ALaser;
+
+UCLASS()
+class AIMMAPSHOOTER_API ASpawningAttach : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawningAttach();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void SpawnRandom();
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerSpawnRandom();
+
+	TArray<AActor*> Actors;
+
+	AHoloScope* Holo;
+
+	AHelmet* Helmet;
+
+	AHeadset* Headset;
+
+	ALaser* Laser;
+
+	AGrip* Grip;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Player")
+	TSubclassOf<AHoloScope> HoloClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Player")
+	TSubclassOf<AHelmet> HelmetClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Player")
+	TSubclassOf<AHeadset> HeadsetClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Player")
+	TSubclassOf<ALaser> LaserClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Player")
+	TSubclassOf<AGrip> GripClass;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
