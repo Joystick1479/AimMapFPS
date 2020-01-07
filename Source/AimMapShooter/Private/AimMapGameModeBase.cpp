e40261bc8a2046067cfa259d18a3ce6ac2768873
barwen1899 <bartek.jj@gmail.com>
2020-01-07 10:01:04 +0000

    Fixed a bug with not spawning pawn. Added semless travel with transition/loading map.

--- Source/AimMapShooter/Private/AimMapGameModeBase.cpp
+++ Source/AimMapShooter/Private/AimMapGameModeBase.cpp
@@ -5,10 +5,23 @@
 
 void AAimMapGameModeBase::PostLogin(APlayerController * NewPlayer)
 {
+	Super::PostLogin(NewPlayer);
+
 	++NumberOfPlayers;
+
+	if (NumberOfPlayers > 1)
+	{
+		UWorld* World = GetWorld();
+		if (!ensure(World != nullptr)) return;
+
+		bUseSeamlessTravel = true;
+		World->ServerTravel("/Game/FirstPersonBP/Maps/Map?listen");
+	}
 }
 
 void AAimMapGameModeBase::Logout(AController * Exiting)
 {
+	Super::Logout(Exiting);
+
 	--NumberOfPlayers;
 }
